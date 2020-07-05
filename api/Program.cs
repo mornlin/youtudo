432d859074dba91bd6604b137936061853eb3767
mornlin <mornlin@163.com>
2020-07-05 14:48:13 +0800

    port 5000

--- api/Program.cs
+++ api/Program.cs
@@ -20,6 +20,7 @@ namespace api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
